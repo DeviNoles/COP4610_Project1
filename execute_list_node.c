682ed4cb9d6faad7f04c57f4c8e520c148610ef4
Dante Coupet <dacoupet@gmail.com>
2020-09-06 13:41:39 -0400

    execute list node function bones

---
+++ execute_list_node.c
@@ -0,0 +1,30 @@
+#include "proj1.h"
+
+void execute_list_node(execution_list** current_node_ptr, execution_list** last_node_ptr) {
+  execution_list* current_node = *current_node_ptr;
+  execution_list* last_node = *last_node_ptr;
+  // Do work depending on which the nodes are
+  if (current_node->type == EXEC_LIST_PROCESS) {
+      string_list* current = current_node->command_and_args;
+      // count the length of the string_list
+      int len = 0;
+      while (current) {
+          len++;
+          current = current->next;
+      }
+      char** argv = (char**) malloc(len*sizeof(char*));
+      // index for argv  
+      int index = 0;
+      current = current_node->command_and_args;
+      while (current) {
+          argv[index++] = current->value;
+          current = current->next;
+      }
+      // at this point, argv contains the necessary process stuff
+      //TODO: actually do execv logic here
+      //TODO: lookup path of command when we use execv 
+
+  }
+  *last_node_ptr = current_node;
+  *current_node_ptr = current_node->next;
+}
--- makefile
+++ makefile
@@ -1,8 +1,9 @@
 headers = proj1.h
-objects = main.o parser.o expand_argument.o build_execution_list.o string_list.o
+objects = main.o parser.o expand_argument.o build_execution_list.o string_list.o \
+	execute_list_node.o
 
-proj1: $(objects)
+shell: $(objects)
 	$(CC) -o $@ $^
 
 clean:
-	rm -f *.o proj1
+	rm -f *.o shell
