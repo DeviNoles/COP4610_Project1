e631777db148261a6e44fdd22a9b4c3d43a0bb84
DevinM6 <devin8821@gmail.com>
2020-09-12 16:34:37 -0400

    Jobs internal function

--- internal_commands_b.c
+++ internal_commands_b.c
@@ -1,5 +1,10 @@
 #include "proj1.h"
 
 void internal_jobs(execution_list *node) {
-
+  node = background_jobs;
+while (node) {
+  printf("[%d] %d ", node->job_id, (long)node->pid);
+  print_string_list(node->command_and_args->next);
+  node = node->next;
+}
 }
