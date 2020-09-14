ee944cc5df2aafe61bb1101339c349d86010571e
Tobe O <thosakwe@gmail.com>
2020-09-13 20:52:18 -0400

    Fix segfault on nonexistent environment variables

--- expand_argument.c
+++ expand_argument.c
@@ -5,52 +5,38 @@
 #include "proj1.h"
 
 char *expand_argument(char *token) {
-  // printf("CALLING EXPAND ARGUMENT");
   if (token[0] == '$') {
-    token++;
-    char *pathString =
-        (char *)malloc(strlen(getenv(token)) + strlen(token) + 1);
-    strcpy(pathString, getenv(token));
-    // printf("%s\n", pathString);
-    // printf("COMMAND : %s\n", getenv(token));
-    return(getenv(token));
+    // Check if the variable exists. If not, return an empty string.
+    char *value = getenv(&token[1]);
+    if (!value) {
+      char* data = malloc(1);
+      data[0] = '\0';
+      return data;
+    } else {
+      return strdup(value);
+    }
   } else if (token[0] == '~') {
-    int length = strlen(getenv("HOME")) + strlen(token)+1;
+    int length = strlen(getenv("HOME")) + strlen(token) + 1;
     char *fullPath = (char *)malloc(length);
     strcpy(fullPath, getenv("HOME"));
     strcat(fullPath, "/");
     if (strlen(token) > 1)
       strcat(fullPath, &token[1]);
-    fullPath[length-1] = 0;
-    // printf("%s\n", fullPath);
+    fullPath[length - 1] = 0;
     return fullPath;
   }
-  /* -arguments
-          else if(token[0]=='-'){
-                  char *tempHolder = (char *) malloc(1 + strlen(fullCommand) +
-     strlen(token) ); char *holder = (char *) malloc(1 + strlen(fullCommand));
-                  strcpy(holder, fullCommand);
-                  strcpy(tempHolder, holder);
-
-                  strcat(tempHolder, token);
-                  fullCommand = tempHolder;
-                  printf("FULL COMMAND");
-          }*/
 
   else {
     char delim[] = ":";
     char *pathString =
         (char *)malloc(strlen(getenv("PATH")) + strlen(token) + 1);
     strcpy(pathString, getenv("PATH"));
-    // printf("PATH IS: %s\n", pathString);
     char *ptr = strtok(pathString, delim);
 
     while (ptr != NULL) {
-      // printf("'%s'\n", ptr);
       ptr = strtok(NULL, delim);
     }
 
-    // free(tempHolder);
     return token;
   }
 }
